c52dc97531d5dfd7302d7e5c25d8d33cfc3d95a7
Ray C Horn <raychorn@gmail.com>
2021-02-14 21:18:14 -0700

    added catch-all for the REST interface.

--- sleepymongoose2/mongoose.py
+++ sleepymongoose2/mongoose.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'], defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def catch_all(path):
+    return 'You want path: %s' % path
+
+if __name__ == '__main__':
+    app.run()
+    
